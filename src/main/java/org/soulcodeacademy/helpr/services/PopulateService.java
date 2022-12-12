49b4da5b282c8e2f9588e6a668ed4cefdfb686e3
Mohotrisk <diegosocial20@hotmail.com>
2022-12-12 17:18:34 -0300

    Adicionado status arquivado no back e ícone do front customizado

--- src/main/java/org/soulcodeacademy/helpr/services/PopulateService.java
+++ src/main/java/org/soulcodeacademy/helpr/services/PopulateService.java
@@ -38,22 +38,26 @@ public class PopulateService {
 
     public void populate() {
         // Integer idCargo, String nome, String descricao, Double salario
-        Cargo c1 = new Cargo(null, "Diretor Geral", "Gerencia a empresa", 30000.0);
-        Cargo c2 = new Cargo(null, "Diretor de Setor", "Gerencia um setor da empresa", 18000.0);
-        Cargo c3 = new Cargo(null, "Técnico geral", "Resolve os chamados urgentes", 12000.0);
+        Cargo c1 = new Cargo(null, "Head", "Gerencia toda a empresa", 30000.0);
+        Cargo c2 = new Cargo(null, "Product Owner", "Coordena o setor de produtos", 18000.0);
+        Cargo c3 = new Cargo(null, "Sênior Analyst", "Resolve os chamados urgentes", 12000.0);
+        Cargo c4 = new Cargo(null,"Head of Marketing","Gerencia  estratégias de marketing",30000.0);
+        Cargo c5 = new Cargo(null, "Head of Product ","Gerencia toda a área de Produtos",30000.0);
+        Cargo c6 = new Cargo(null,"Head of D&I","Elabora políticas da empresa que reforçam a diversidade no local de trabalho", 30000.0);
+
 
         // Integer id, String nome, String email, String cpf, String senha, String foto, Cargo cargo
-        Funcionario f1 = new Funcionario(null, "Renato Pereira", "renato.pereira@gmail.com", "68258098144", encoder.encode("12345"), null, c1);
+        Funcionario f1 = new Funcionario(null, "Renato Pereira", "renato.pereira@gmail.com", "68258098144", encoder.encode("12345"), null, c5);
         f1.setPerfil(Perfil.ADMIN);
         Funcionario f2 = new Funcionario(null, "Victor Icoma", "victor.icoma@gmail.com", "51127383671", encoder.encode("12345"), null, c2);
-        Funcionario f3 = new Funcionario (null, "Anitta Zaz", "Anitta.zaz@gmail.com", "44968914687", encoder.encode("12345"), null, c3);
-        Funcionario f4 = new Funcionario (null,"Jenifer Lopes","Jenifer.lpz@gmail.com","68237198778", encoder.encode("12345"), null, c1);
+        Funcionario f3 = new Funcionario (null, "Anitta Zaz", "Anitta.zaz@gmail.com", "44968914687", encoder.encode("12345"), null, c4);
+        Funcionario f4 = new Funcionario (null,"Jenifer Guzman","Jenifer.gzm@gmail.com","68237198778", encoder.encode("12345"), null, c6);
         f4.setPerfil(Perfil.ADMIN);
-        Funcionario f5 = new Funcionario (null, "Harry Styles", "Harry.stls@gmail.com", "86470827101", encoder.encode("12345"),null,c2 );
+        Funcionario f5 = new Funcionario (null, "Harry Lemes", "Harry.lm@gmail.com", "86470827101", encoder.encode("12345"),null,c3 );
         f5.setPerfil(Perfil.ADMIN);
-        Funcionario f6 = new Funcionario(null, "Alceu Valença", "Alceu.vlc@gmail.com","24365648683", encoder.encode("12345"),null,c3 );
+        Funcionario f6 = new Funcionario(null, "Alceu Silva", "Alceu.vlc@gmail.com","24365648683", encoder.encode("12345"),null,c3 );
         f6.setPerfil(Perfil.ADMIN);
-        Funcionario f7 = new Funcionario(null, "Lazaro Ramos", "Lazaro.rms@gmail.com","91733168494", encoder.encode("12345"),null, c1 );
+        Funcionario f7 = new Funcionario(null, "Lazaro Petković", "Lazaro.ptk@gmail.com","91733168494", encoder.encode("12345"),null, c1 );
         f7.setPerfil(Perfil.ADMIN);
 
 
@@ -61,9 +65,9 @@ public class PopulateService {
 
         Cliente cl1 = new Cliente(null, "José Almir", "jose.almir@gmail.com", "12659185115", encoder.encode("batata"), "011963322145");
         Cliente cl2 = new Cliente(null, "Pedro João", "pedro.joão@gmail.com", "37734168302", encoder.encode("batata"), "011941414141");
-        Cliente cl3 = new Cliente(null,"Emma Stone","Emma.Stone@gmail.com","95255461105", encoder.encode("batata"), "011963636363");
-        Cliente cl4 = new Cliente(null,"Camila de Lucas", "Camila.lucas@gmail.com", "21620573954", encoder.encode("batata"),"011987878787" );
-        Cliente cl5 = new Cliente(null, "Erika Hilton", "Erika.Hilton@gmail.com", "87220874359", encoder.encode("batata"),"011963366556" );
+        Cliente cl3 = new Cliente(null,"Emma Stone","emma.stone@gmail.com","95255461105", encoder.encode("batata"), "011963636363");
+        Cliente cl4 = new Cliente(null,"Camila de Lucas", "camila.lucas@gmail.com", "21620573954", encoder.encode("batata"),"011987878787" );
+        Cliente cl5 = new Cliente(null, "Erika Hilton", "erika.hilton@gmail.com", "87220874359", encoder.encode("batata"),"011963366556" );
 
         FuturoCandidato fc1 = new FuturoCandidato(null, "Diego", "diego@gmail.com", "candidato para vaga de Manutenção", Setor.MANUTENCAO);
         FuturoCandidato fc2 = new FuturoCandidato(null, "Rafael", "rafael@gmail.com", "candidato para vaga de RH", Setor.RECURSOS_HUMANOS);
@@ -72,24 +76,24 @@ public class PopulateService {
         Chamado ch1 = new Chamado(null, "Primeiro chamado do sistema", "Revisar as entidades criadas");
         ch1.setCliente(cl1);
 
-        Chamado ch2 = new Chamado(null, "Ativar VPN do sistema", "Conectar aos servidores remotos");
+        Chamado ch2 = new Chamado(null, "Ativar VPN do sistema", "Permitir que todas as máquinas se conectem aos servidores remotos");
         ch2.setCliente(cl2);
-        ch2.setFuncionario(f1);
+        ch2.setFuncionario(f6);
         ch2.setStatus(StatusChamado.ATRIBUIDO);
 
-        Chamado ch3 = new Chamado(null,"Desbloqueio de usuário","Senha bloqueada por excesso de tentativas" );
+        Chamado ch3 = new Chamado(null,"Desbloqueio de usuários","Desbloquear todas as senhas bloqueadas por excesso de tentativas" );
         ch3.setCliente(cl5);
         ch3.setStatus(StatusChamado.RECEBIDO);
 
-        Chamado ch4 = new Chamado(null,"Sistema operacional", "Instalação do sistema operacional Windows" );
+        Chamado ch4 = new Chamado(null,"Compra de Macbooks Pro 13", "Orçamento e previsão para compra de Macbook Pro 13 para todo setor de marketing" );
         ch4.setCliente(cl5);
         ch4.setStatus(StatusChamado.RECEBIDO);
 
-        Chamado ch5 = new Chamado(null,"Sistema operacional", "Instalação do sistema operacional Linux" );
+        Chamado ch5 = new Chamado(null,"Instalação de Linux", "Instalação do sistema operacional Linux em 50% das máquinas" );
         ch5.setCliente(cl3);
         ch5.setStatus(StatusChamado.RECEBIDO);
 
-        Chamado ch6 = new Chamado(null,"Antivírus off", "Cadastrar chave de segurança no antivírus da máquina" );
+        Chamado ch6 = new Chamado(null,"Campanha December 12%", "Enviar email com cupon de 12% de desconto para todos clientes da base" );
         ch6.setCliente(cl4);
         ch6.setFuncionario(f3);
         ch6.setStatus(StatusChamado.CONCLUIDO);
@@ -97,16 +101,23 @@ public class PopulateService {
         Chamado ch7 = new Chamado(null,"Monitor Queimado", "Liberar um novo monitor para funcionário");
         ch7.setCliente(cl4);
 
-        Chamado ch8 = new Chamado(null,"Limpeza de disco", "Funcionamento da máquina está lento" );
+        Chamado ch8 = new Chamado(null,"Contratação", "Contratar novos funcionarios para vagas afirmativas" );
         ch8.setCliente(cl5);
-        ch8.setFuncionario(f7);
+        ch8.setFuncionario(f4);
         ch8.setStatus(StatusChamado.CONCLUIDO);
 
+        Chamado ch9 = new Chamado(null,"Compra de novos monitores", "Orçamento e previsão de compra de novos monitores pra toda empresa" );
+        ch9.setCliente(cl3);
+        ch9.setFuncionario(f7);
+        ch9.setStatus(StatusChamado.ARQUIVADO);
+
+
+
         // vamos persistir as entidades = salvar no banco
-        this.cargoRepository.saveAll(List.of(c1, c2, c3));
+        this.cargoRepository.saveAll(List.of(c1, c2, c3, c4, c5, c6));
         this.funcionarioRepository.saveAll(List.of(f1, f2, f3, f4, f5, f6, f7));
         this.clienteRepository.saveAll(List.of(cl1, cl2, cl2, cl3, cl4, cl5));
-        this.chamadoRepository.saveAll(List.of(ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8));
+        this.chamadoRepository.saveAll(List.of(ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9));
         this.futuroCandidatoRepository.saveAll(List.of(fc1, fc2, fc3));
 
     }
